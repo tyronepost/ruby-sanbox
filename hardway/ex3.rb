5c9733519f9df096bd910acde12dde8d22d16556
tyronepost <tyrone.post.a@gmail.com>
2016-10-13 07:35:12 -0400

    comments exercise

--- hardway/ex3.rb
+++ hardway/ex3.rb
@@ -1,24 +1,32 @@
+# outputs a string
 puts "I will now count my chickens"
 
+# calculates a value and outputs a string using string interpolation
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+# outputs another string
 puts "Now I will count the eggs:"
 
+# outputs a value calulated with a bunch of operators
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# outputs a string
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# calculates a value and outputs it as a string
 puts 3 + 2 < 5 - 7
 
+# string interpolation
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
 
+# string output
 puts "Oh, thats why it's false."
 
 puts "How about some more."
 
+# calcutes booleans and outputs them as strings
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
-
