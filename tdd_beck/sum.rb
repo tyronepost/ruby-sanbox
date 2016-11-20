f74bd168f0b7e391aece884ec856657e8cae8fb0
tyronepost <tyrone.post.a@gmail.com>
2016-11-20 13:40:56 -0500

    adding classes for reducing to a given currency

---
+++ tdd_beck/sum.rb
@@ -0,0 +1,11 @@
+class Sum
+  def initialize(augend, addend)
+    @augend = augend
+    @addend = addend
+  end
+
+  def reduce(to)
+    amount = @augend.amount + @addend.amount
+    Money.new(amount, to)
+  end
+end
--- tdd_beck/test_money.rb
+++ tdd_beck/test_money.rb
@@ -1,4 +1,7 @@
-require_relative 'money'
+require_relative 'bank'
+require_relative 'money' 
+require_relative 'sum'
+
 require 'test/unit'
 
 class TestMoney < Test::Unit::TestCase
@@ -6,6 +9,22 @@ class TestMoney < Test::Unit::TestCase
     five = Money.dollar(5)
     assert_equal(Money.dollar(10), five.times(2))
     assert_equal(Money.dollar(15), five.times(3))
+  end
+
+  def test_addition
+    five = Money.dollar(5)
+    sum = Sum.new(five, five)
+    bank = Bank.new
+    reduced = bank.reduce(sum, "USD")
+    ten = Money.dollar(10)
+    assert_equal(ten, reduced)
+  end
+
+  def test_reduce_sum
+    sum = Sum.new(Money.dollar(3), Money.dollar(4))
+    bank = Bank.new
+    result = bank.reduce(sum, "USD")
+    assert_equal(Money.dollar(7), result)
   end
 
   def test_franc_multiplication
@@ -18,6 +37,7 @@ class TestMoney < Test::Unit::TestCase
     assert_equal("USD", Money.dollar(1).currency)
     assert_equal("CHF", Money.franc(1).currency)
   end
+
 
   def test_equality
     assert_true(Money.dollar(5) == Money.dollar(5))
