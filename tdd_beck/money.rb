7e3f9ab15f612a7a740277166192f50aaace392e
tyronepost <tyrone.post.a@gmail.com>
2016-11-20 06:25:30 -0500

    created "static" factory methods for dollar and franc

--- tdd_beck/money.rb
+++ tdd_beck/money.rb
@@ -1,6 +1,14 @@
 class Money
   attr_reader :amount
 
+  def self.dollar(amount)
+    Dollar.new(amount)
+  end
+
+  def self.franc(amount)
+    Franc.new(amount)
+  end
+
   def ==(other)
     amount == other.amount && self.class == other.class
   end
--- tdd_beck/test_money.rb
+++ tdd_beck/test_money.rb
@@ -4,22 +4,22 @@ require 'test/unit'
 
 class TestMoney < Test::Unit::TestCase
   def test_multiplication
-    five = Dollar.new(5)
-    assert_equal(Dollar.new(10), five.times(2))
-    assert_equal(Dollar.new(15), five.times(3))
+    five = Money.dollar(5)
+    assert_equal(Money.dollar(10), five.times(2))
+    assert_equal(Money.dollar(15), five.times(3))
   end
 
   def test_franc_multiplication
-    five = Franc.new(5)
-    assert_equal(Franc.new(10), five.times(2))
-    assert_equal(Franc.new(15), five.times(3))
+    five = Money.franc(5)
+    assert_equal(Money.franc(10), five.times(2))
+    assert_equal(Money.franc(15), five.times(3))
   end
 
   def test_equality
-    assert_true(Dollar.new(5) == Dollar.new(5))
-    assert_false(Dollar.new(5) == Dollar.new(6))
-    assert_true(Franc.new(5) == Franc.new(5))
-    assert_false(Franc.new(5) == Franc.new(6))
-    assert_false(Dollar.new(5) == Franc.new(5))
+    assert_true(Money.dollar(5) == Money.dollar(5))
+    assert_false(Money.dollar(5) == Money.dollar(6))
+    assert_true(Money.franc(5) == Money.franc(5))
+    assert_false(Money.franc(5) == Money.franc(6))
+    assert_false(Money.dollar(5) == Money.franc(5))
   end
 end
